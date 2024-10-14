1037a0514b046cc7e315d13b2ed76859ba329825
quentinut <quentin@unique-ticket.com>
2024-10-14 13:45:21 +0200

    feat: companies.services unit test > 2 errors

--- src/companies/companies.service.spec.ts
+++ src/companies/companies.service.spec.ts
@@ -1,17 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CompaniesService } from './companies.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { Company } from './entities/company.entity';
 import { CrmUser } from '../crm_users/entities/crm_user.entity';
 import { CrmUsersMemberOfCompany } from '../crm-users-member-of-companies/entities/crm-users-member-of-company.entity';
+import { Repository } from 'typeorm';
+import { CreateCompanyDto } from './dto/create-company.dto';
+import { UpdateCompanyDto } from './dto/update-company.dto';
 import { NotFoundException } from '@nestjs/common';
 
 describe('CompaniesService', () => {
   let service: CompaniesService;
-  let companyRepository: jest.Mocked<Repository<Company>>;
-  let crmUserRepository: jest.Mocked<Repository<CrmUser>>;
-  let crmUsersMemberOfCompanyRepository: jest.Mocked<Repository<CrmUsersMemberOfCompany>>;
+  let companyRepository: Repository<Company>;
+  let crmUserRepository: Repository<CrmUser>;
+  let crmUsersMemberOfCompanyRepository: Repository<CrmUsersMemberOfCompany>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -19,133 +21,199 @@ describe('CompaniesService', () => {
         CompaniesService,
         {
           provide: getRepositoryToken(Company),
-          useValue: {
-            create: jest.fn(),
-            save: jest.fn(),
-            findOne: jest.fn(),
-            remove: jest.fn(),
-          },
+          useClass: Repository,
         },
         {
           provide: getRepositoryToken(CrmUser),
-          useValue: {
-            findOne: jest.fn(),
-          },
+          useClass: Repository,
         },
         {
           provide: getRepositoryToken(CrmUsersMemberOfCompany),
-          useValue: {
-            create: jest.fn(),
-            save: jest.fn(),
-            delete: jest.fn(),
-          },
+          useClass: Repository,
         },
       ],
     }).compile();
 
     service = module.get<CompaniesService>(CompaniesService);
-    companyRepository = module.get(getRepositoryToken(Company));
-    crmUserRepository = module.get(getRepositoryToken(CrmUser));
-    crmUsersMemberOfCompanyRepository = module.get(getRepositoryToken(CrmUsersMemberOfCompany));
+    companyRepository = module.get<Repository<Company>>(
+      getRepositoryToken(Company)
+    );
+    crmUserRepository = module.get<Repository<CrmUser>>(
+      getRepositoryToken(CrmUser)
+    );
+    crmUsersMemberOfCompanyRepository = module.get<
+      Repository<CrmUsersMemberOfCompany>
+    >(getRepositoryToken(CrmUsersMemberOfCompany));
   });
 
-  describe('CompaniesService - create', () => {
-    let createCompanyDto: any;
-    let savedCompany: any;
-    let mockCreator: CrmUser;
-    let mockMemberOfCompany: CrmUsersMemberOfCompany;
-  
-    beforeEach(() => {
-      // Données pour la création de la compagnie
-      createCompanyDto = {
-        name: 'New Company',
-        companyLogo: { name: 'logo.png', url: 'http://example.com/logo.png' },
-        primaryColor: '#FF5733',
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a company and link the creator', async () => {
+      const createCompanyDto: CreateCompanyDto = {
+        name: 'Test Company',
+        companyLogo: { name: 'logo', url: 'url' },
+        primaryColor: '#FFFFFF',
         accountSubscriptionStatus: 'active',
-        stripeAccountId: 'acct_1234567890abcdef',
-        creatorId: 1,  // Utilisé uniquement pour la création
+        stripeAccountId: 'acct_1234',
+        creatorId: 1,
       };
-  
-      // Données du créateur
-      mockCreator = { id: 1, firstname: 'John', lastname: 'Doe' } as CrmUser;
-  
-      // Compagnie simulée sauvegardée
-      savedCompany = {
+
+      const creator = { id: 1 } as CrmUser;
+      const createdCompany = {
         id: 1,
-        name: 'New Company',
-        companyLogo: { name: 'logo.png', url: 'http://example.com/logo.png' },
-        primaryColor: '#FF5733',
-        accountSubscriptionStatus: 'active',
-        stripeAccountId: 'acct_1234567890abcdef',
-      };
-  
-      // Membre de la compagnie simulé
-      mockMemberOfCompany = {
-        crmUser: mockCreator,
-        company: { id: 1 } as Company,
-        accessLevel: 'admin',
-        permissions: ['all'],
-      } as CrmUsersMemberOfCompany;
-    });
-  
-    it('should create a new company and add the creator as a member', async () => {
-      // Simuler la récupération du créateur
-      crmUserRepository.findOne.mockResolvedValue(mockCreator);
-      
-      // Simuler la création et la sauvegarde de la compagnie sans `creatorId`
-      companyRepository.create.mockReturnValue(savedCompany as any);
-      companyRepository.save.mockResolvedValue(savedCompany as any);
-  
-      // Simuler la création et la sauvegarde dans `CrmUsersMemberOfCompany`
-      crmUsersMemberOfCompanyRepository.create.mockReturnValue(mockMemberOfCompany);
-      crmUsersMemberOfCompanyRepository.save.mockResolvedValue(mockMemberOfCompany);
-  
-      // Appeler la méthode create du service
+        ...createCompanyDto,
+        events: [],
+        members: [],
+        followers: [],
+        reports: [],
+      } as Company;
+
+      jest.spyOn(crmUserRepository, 'findOne').mockResolvedValue(creator);
+      jest.spyOn(companyRepository, 'create').mockReturnValue(createdCompany); // Mock correct de create
+      jest.spyOn(companyRepository, 'save').mockResolvedValue(createdCompany);
+      jest
+        .spyOn(crmUsersMemberOfCompanyRepository, 'save')
+        .mockResolvedValue(null);
+
       const result = await service.create(createCompanyDto);
-  
-      // Vérifier que le résultat ne contient pas `creatorId`
-      expect(result).toEqual(savedCompany);
-  
-      // Vérifier que les méthodes ont été appelées correctement
-      expect(crmUserRepository.findOne).toHaveBeenCalledWith({ where: { id: 1 } });
-      expect(companyRepository.create).toHaveBeenCalledWith({
-        name: 'New Company',
-        companyLogo: { name: 'logo.png', url: 'http://example.com/logo.png' },
-        primaryColor: '#FF5733',
-        accountSubscriptionStatus: 'active',
-        stripeAccountId: 'acct_1234567890abcdef',
+
+      expect(result).toEqual(createdCompany);
+      expect(crmUserRepository.findOne).toHaveBeenCalledWith({
+        where: { id: createCompanyDto.creatorId },
       });
-      expect(companyRepository.save).toHaveBeenCalledWith(savedCompany);
-      expect(crmUsersMemberOfCompanyRepository.create).toHaveBeenCalledWith({
-        crmUser: mockCreator,
-        company: savedCompany,
-        accessLevel: 'admin',
-        permissions: ['all'],
-      });
-      expect(crmUsersMemberOfCompanyRepository.save).toHaveBeenCalledWith(mockMemberOfCompany);
+      expect(companyRepository.create).toHaveBeenCalledWith(expect.any(Object)); // Vérification de create
+      expect(companyRepository.save).toHaveBeenCalledWith(expect.any(Object));
+      expect(crmUsersMemberOfCompanyRepository.save).toHaveBeenCalledWith(
+        expect.any(Object)
+      );
     });
   });
-  
+
+  describe('findAll', () => {
+    it('should return an array of companies', async () => {
+      const companies = [
+        {
+          id: 1,
+          name: 'Test Company',
+          companyLogo: { name: 'logo', url: 'url' },
+          primaryColor: '#FFFFFF',
+          accountSubscriptionStatus: 'active',
+          stripeAccountId: 'acct_1234',
+          events: [],
+          members: [],
+          followers: [],
+          reports: [],
+        },
+      ];
+
+      jest.spyOn(companyRepository, 'find').mockResolvedValue(companies);
+
+      const result = await service.findAll();
+      expect(result).toEqual(companies);
+      expect(companyRepository.find).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a company if found', async () => {
+      const company = {
+        id: 1,
+        name: 'Test Company',
+        companyLogo: { name: 'logo', url: 'url' },
+        primaryColor: '#FFFFFF',
+        accountSubscriptionStatus: 'active',
+        stripeAccountId: 'acct_1234',
+        events: [],
+        members: [],
+        followers: [],
+        reports: [],
+      };
+
+      jest.spyOn(companyRepository, 'findOne').mockResolvedValue(company);
+
+      const result = await service.findOne(1);
+      expect(result).toEqual(company);
+      expect(companyRepository.findOne).toHaveBeenCalledWith({
+        where: { id: 1 },
+      });
+    });
+
+    it('should throw NotFoundException if the company is not found', async () => {
+      jest.spyOn(companyRepository, 'findOne').mockResolvedValue(null);
+
+      await expect(service.findOne(1)).rejects.toThrow(NotFoundException);
+      expect(companyRepository.findOne).toHaveBeenCalledWith({
+        where: { id: 1 },
+      });
+    });
+  });
+
+  describe('update', () => {
+    it('should update a company', async () => {
+      const updateCompanyDto: UpdateCompanyDto = { name: 'Updated Company' };
+
+      const company = {
+        id: 1,
+        name: 'Test Company',
+        companyLogo: { name: 'logo', url: 'url' },
+        primaryColor: '#FFFFFF',
+        accountSubscriptionStatus: 'active',
+        stripeAccountId: 'acct_1234',
+        events: [],
+        members: [],
+        followers: [],
+        reports: [],
+      };
+
+      const updatedCompany = { ...company, ...updateCompanyDto };
+
+      jest.spyOn(service, 'findOne').mockResolvedValue(company);
+      jest.spyOn(companyRepository, 'save').mockResolvedValue(updatedCompany);
+
+      const result = await service.update(1, updateCompanyDto);
+
+      expect(result).toEqual(updatedCompany);
+      expect(service.findOne).toHaveBeenCalledWith(1);
+      expect(companyRepository.save).toHaveBeenCalledWith(updatedCompany);
+    });
+  });
 
   describe('remove', () => {
-    it('should delete a company and return it', async () => {
-      const company = { id: 1, name: 'Company A' } as Company;
+    it('should remove a company and its relations', async () => {
+      const company = {
+        id: 1,
+        name: 'Test Company',
+        companyLogo: { name: 'logo', url: 'url' },
+        primaryColor: '#FFFFFF',
+        accountSubscriptionStatus: 'active',
+        stripeAccountId: 'acct_1234',
+        events: [],
+        members: [],
+        followers: [],
+        reports: [],
+      };
 
-      jest.spyOn(companyRepository, 'findOne').mockResolvedValue(company); // Correct the findOne mock
-      jest.spyOn(crmUsersMemberOfCompanyRepository, 'delete').mockResolvedValue(undefined);
+      jest.spyOn(service, 'findOne').mockResolvedValue(company);
+      jest.spyOn(companyRepository, 'findOne').mockResolvedValue(company); // Mock pour findOne
+      jest
+        .spyOn(crmUsersMemberOfCompanyRepository, 'delete')
+        .mockResolvedValue(null);
       jest.spyOn(companyRepository, 'remove').mockResolvedValue(company);
 
       const result = await service.remove(1);
 
       expect(result).toEqual(company);
-      expect(crmUsersMemberOfCompanyRepository.delete).toHaveBeenCalledWith({ company: { id: 1 } });
+      expect(service.findOne).toHaveBeenCalledWith(1); // Vérifie que findOne a bien été appelée
+      expect(companyRepository.findOne).toHaveBeenCalledWith({
+        where: { id: 1 },
+      });
+      expect(crmUsersMemberOfCompanyRepository.delete).toHaveBeenCalledWith({
+        company: { id: 1 },
+      });
       expect(companyRepository.remove).toHaveBeenCalledWith(company);
-    });
-
-    it('should throw NotFoundException if company is not found', async () => {
-      jest.spyOn(companyRepository, 'findOne').mockResolvedValue(undefined); // Mocking findOne to return undefined
-
-      await expect(service.remove(999)).rejects.toThrow(NotFoundException);
     });
   });
 });
